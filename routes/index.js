1148bee4dbfb9b9746516a72e5d88bbb84617559
zida-bi <zida_bi@hotmail.com>
2015-03-27 19:40:30 -0400

    Second template, header and footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Assignment03' });
+});
+
+/* GET other pages. */
+router.get('/aboutme.html', function(req, res, next) {
+  res.render('other', { title: 'About Me' });
 });
 
 module.exports = router;
