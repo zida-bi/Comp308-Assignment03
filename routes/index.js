8dac4602a6492860447bfc876e44d9c3cfa696e7
zida-bi <zida_bi@hotmail.com>
2015-03-27 21:26:29 -0400

    Contact me page done

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,14 @@ router.get('/aboutme.html', function(req, res, next) {
   res.render('other', { title: 'About Me' });
 });
 
-/* GET projects pages. */
+/* GET projects page. */
 router.get('/projects.html', function(req, res, next) {
   res.render('other', { title: 'My Projects' });
+});
+
+/* GET contace me page. */
+router.get('/contactme.html', function(req, res, next) {
+  res.render('other', { title: 'Trying to Build Contact With Me?' });
 });
 
 module.exports = router;
